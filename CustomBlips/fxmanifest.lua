e6172e9e944fec1ff25a7da811d43535374060fd
Ex_method <115433269+Exmethod@users.noreply.github.com>
2023-09-17 15:56:16 -0400

    Add files via upload

---
+++ CustomBlips/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Ex_method'
+description 'Custom Blips'
+version '1.0.0'
+
+files({
+	"blips/blips.lua",
+})
+
+client_script 'blips/blips.lua'
+server_script 'server/server.lua'
