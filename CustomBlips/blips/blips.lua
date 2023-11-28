9dfcddcdc3d0b3b7774903b88d260c3c17bb805d
Ex_method <115433269+Exmethod@users.noreply.github.com>
2023-11-28 10:39:34 -0500

    Update blips.lua

--- CustomBlips/blips/blips.lua
+++ CustomBlips/blips/blips.lua
@@ -8,8 +8,8 @@
 -- Cfx Blips website: https://docs.fivem.net/docs/game-references/blips/
 local blips = {
 
-     {title="Example", colour=52, id=141, x = 1737.1256, y = 3027.9854, z = 67.9953},
-     {title="Example2", colour=51, id=142, x = 1757.1256, y = 3017.9854, z = 65.9953}
+     {title="Example", size=1.0, colour=52, id=141, category=, x = 1737.1256, y = 3027.9854, z = 67.9953},
+     {title="Example2", size=1.0, colour=51, id=142, category=, x = 1757.1256, y = 3017.9854, z = 65.9953}
 }
 
 -- Do not touch any thing under this
@@ -19,10 +19,11 @@ Citizen.CreateThread(function()
       info.blip = AddBlipForCoord(info.x, info.y, info.z)
       SetBlipSprite(info.blip, info.id)
       SetBlipDisplay(info.blip, 2)
-      SetBlipScale(info.blip, 1.0)
+      SetBlipScale(info.blip, info.size)
+      SetBlipCategory(info.blip, info.category)
       SetBlipColour(info.blip, info.colour)
       SetBlipAsShortRange(info.blip, true)
-	  BeginTextCommandSetBlipName("STRING")
+      BeginTextCommandSetBlipName("STRING")
       AddTextComponentString(info.title)
       EndTextCommandSetBlipName(info.blip)
     end
