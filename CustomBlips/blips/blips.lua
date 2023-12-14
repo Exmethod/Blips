dd5afb021064c0d954225a5081b16a4a5b4112c5
Ex_method <115433269+Exmethod@users.noreply.github.com>
2023-12-13 21:22:07 -0500

    Update blips.lua

--- CustomBlips/blips/blips.lua
+++ CustomBlips/blips/blips.lua
@@ -6,10 +6,11 @@
 -- ╚═══╩╝╚╩╩╩╩══╩═╩╝╚╩══╩══╝
 -- Add as much stuff as you need
 -- Cfx Blips website: https://docs.fivem.net/docs/game-references/blips/
+-- Make Sure all of the Blips have spaces whe it say example= 1,
 local blips = {
 
-     {title="Example", size=1.0, colour=52, id=141, category=example, x = 1737.1256, y = 3027.9854, z = 67.9953},
-     {title="Example2", size=1.0, colour=51, id=142, category=example, x = 1757.1256, y = 3017.9854, z = 65.9953}
+     {title="Example", size=1.0, colour=52, id=141, category= example, x = 1737.1256, y = 3027.9854, z = 67.9953},
+     {title="Example2", size=1.0, colour=51, id=142, category= example, x = 1757.1256, y = 3017.9854, z = 65.9953}
 }
 
 -- Do not touch any thing under this
